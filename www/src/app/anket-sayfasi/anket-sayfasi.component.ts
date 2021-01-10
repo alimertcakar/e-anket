ab51a0125fc6a7e39a59d59a724edf2ff3a83cfb
Ali Mert Çakar <mail@alimert.net>
2021-01-10 15:57:06 +0300

    FEAT Cevaplama

--- www/src/app/anket-sayfasi/anket-sayfasi.component.ts
+++ www/src/app/anket-sayfasi/anket-sayfasi.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Action } from 'rxjs/internal/scheduler/Action';
+import { AnketService } from '../anket.service';
 
 @Component({
   selector: 'app-anket-sayfasi',
   templateUrl: './anket-sayfasi.component.html',
-  styleUrls: ['./anket-sayfasi.component.scss']
+  styleUrls: ['./anket-sayfasi.component.scss'],
 })
 export class AnketSayfasiComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute, private anket: AnketService) {}
+  anketDetayi: any;
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(async (params) => {
+      let anketId = params.get('id');
+      let anketDetay = await this.anket.getAnket(anketId);
+      anketDetay.subscribe((data) => {
+        this.anketDetayi = data;
+      });
+    });
   }
-
+  cevapla(i) {
+    console.log(i);
+  }
 }
