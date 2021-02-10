da124e0ccb402079b3604380d219fb7d421b15ff
Ali Mert Çakar <mail@alimert.net>
2021-02-10 22:08:50 +0300

    TEXT - CODE karşılama mesajı

--- www/src/app/auth.service.ts
+++ www/src/app/auth.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor() {}
+
+  async getUser() {
+    var user = firebase.auth().currentUser;
+    console.log(user);
+    if (user) {
+      return user;
+    } else {
+      return 'giriş yapılmadı';
+    }
+  }
 }
--- www/src/app/login-register/login-register.component.ts
+++ www/src/app/login-register/login-register.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login-register',
   templateUrl: './login-register.component.html',
   styleUrls: ['./login-register.component.scss'],
 })
 export class LoginRegisterComponent implements OnInit {
-  constructor(public auth: AngularFireAuth) {}
+  loggedInUser: string | any;
+  constructor(public auth: AngularFireAuth, private authService: AuthService) {}
   ngOnInit(): void {}
 
   login() {
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.authService.getUser().then((x) => (this.loggedInUser = x));
   }
   loginEmail(login) {
     const { email, password } = login.form.value;
@@ -24,5 +27,8 @@ export class LoginRegisterComponent implements OnInit {
   }
   logout(email, password) {
     this.auth.signOut();
+  }
+  loginGmail() {
+    // this.authService.getUser().then((x) => (this.loggedInUser = x));
   }
 }
